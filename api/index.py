36a14fcab9c896286620cde28dd2fdafe63b409c
Josh Finch <me@joshfinch.com>
2024-02-20 21:52:12 +0000

    Adds temporary spaceapi.json placeholder

--- api/index.py
+++ api/index.py
@@ -2,10 +2,20 @@ from flask import Flask
 
 app = Flask(__name__)
 
-@app.route('/')
-def home():
-    return 'Hello, World!'
 
-@app.route('/about')
-def about():
-    return 'About'
+@app.route("/spaceapi.json")
+def home():
+    return {
+        "api_compatibility": ["14"],
+        "space": "LDSHack",
+        "logo": "https://www.ldshack.org/img/logo.svg",
+        "url": "https://ldshack.org",
+        "contact": {
+            "email": "info@ldshack.org",
+        },
+        "issue_report_channels": ["email"],
+        "state": {
+            "open": False,
+            "message": "Work in progress"
+        }
+    }
